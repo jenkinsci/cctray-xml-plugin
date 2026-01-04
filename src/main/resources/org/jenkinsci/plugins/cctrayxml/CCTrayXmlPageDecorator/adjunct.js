1ede82a241a0d1fb24cbe79838fd3a0ddbed75b9
Daniel Beck <daniel-beck@users.noreply.github.com>
2026-01-04 16:46:06 +0100

    [JENKINS-74363] Make plugin compatible with Jenkins 2.539+ CSP

---
+++ src/main/resources/org/jenkinsci/plugins/cctrayxml/CCTrayXmlPageDecorator/adjunct.js
@@ -0,0 +1,6 @@
+Behaviour.addLoadEvent(function() {
+        var rssbar = document.getElementById('rss-bar')
+        if (typeof rssbar !== 'undefined' && rssbar !== null) {
+            rssbar.insertAdjacentHTML('beforeend', ` <a href="cc.xml/" class="yui-button link-button"><img src="${resURL}/plugin/cctray-xml/images/svgs/Icon-txt.svg" class="leading-icon" height="16px"/> cc.xml</a> <a href="cc.xml/?recursive" class="yui-button link-button"><img src="${resURL}/plugin/cctray-xml/images/svgs/Icon-txt.svg" class="leading-icon" height="16px"/> recursive cc.xml</a></a>`);
+        }
+});
