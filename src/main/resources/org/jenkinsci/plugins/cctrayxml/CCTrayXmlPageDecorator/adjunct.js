947590fd2ffe446a5be06975ed2074cb3287a242
Rahul Somasunderam <rahul.som@gmail.com>
2026-01-04 21:24:58 -0800

    refactor: Format injected HTML
    This makes working with it easier.
    
    Also, remove redundant `</a>`.

--- src/main/resources/org/jenkinsci/plugins/cctrayxml/CCTrayXmlPageDecorator/adjunct.js
+++ src/main/resources/org/jenkinsci/plugins/cctrayxml/CCTrayXmlPageDecorator/adjunct.js
@@ -1,6 +1,14 @@
 Behaviour.addLoadEvent(function() {
         var rssbar = document.getElementById('rss-bar')
         if (typeof rssbar !== 'undefined' && rssbar !== null) {
-            rssbar.insertAdjacentHTML('beforeend', ` <a href="cc.xml/" class="yui-button link-button"><img src="${resURL}/plugin/cctray-xml/images/svgs/Icon-txt.svg" class="leading-icon" height="16px"/> cc.xml</a> <a href="cc.xml/?recursive" class="yui-button link-button"><img src="${resURL}/plugin/cctray-xml/images/svgs/Icon-txt.svg" class="leading-icon" height="16px"/> recursive cc.xml</a></a>`);
+            rssbar.insertAdjacentHTML('beforeend', `
+                <a href="cc.xml/" class="yui-button link-button">
+                  <img src="${resURL}/plugin/cctray-xml/images/svgs/Icon-txt.svg"
+                      class="leading-icon" height="16px"/> cc.xml
+                </a>
+                <a href="cc.xml/?recursive" class="yui-button link-button">
+                  <img src="${resURL}/plugin/cctray-xml/images/svgs/Icon-txt.svg"
+                      class="leading-icon" height="16px"/> recursive cc.xml
+                </a>`);
         }
 });
